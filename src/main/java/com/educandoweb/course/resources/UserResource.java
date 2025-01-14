1e6e4f564fa8757aa673f4668d4b9e717b413914
joao.murari <joao_pichula@hotmail.com>
2025-01-14 14:05:46 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") // CRIA O ENDPOINT EX: Localhost:8080/users. abre os users
 public class UserResource {
 	
-	@GetMapping
+	@GetMapping // Mapeia usando response, para buscar os usuarios
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L,"Maria", "Maria@gmail.com", "99999","12345");
 		return ResponseEntity.ok().body(u);
